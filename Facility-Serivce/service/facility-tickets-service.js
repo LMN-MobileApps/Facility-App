190d03f2cb0eb135bd73060ee06bf9e5c5a12838
rakshahm20 <96164301+rakshahm20@users.noreply.github.com>
2022-01-17 16:10:34 +0530

    Update facility-tickets-service.js

--- Facility-Serivce/service/facility-tickets-service.js
+++ Facility-Serivce/service/facility-tickets-service.js
@@ -2,7 +2,14 @@ const TicketRepository = require("../repository/facility-tickets-repository");
 const MasterDataRepository = require('../repository/master-data-repository');
 userRole = require('../assets/user-role.json');
 
-resArray=[]
+result=[];
+resArray=[];
+empArray=[];
+assignedToMeCount=0;
+pendingTicketsCount=0;
+resolvedTicketsCount=0;
+rejectedTicketsCount=0;
+
 module.exports = class TicketService {
 
     getAllTickets(callback) {
@@ -57,6 +64,89 @@ module.exports = class TicketService {
                     "RejectedTickets": rejectedTickets
                 });
             });
+        });
+    }
+    
+    dashboard(id,callback){
+        assignedToMeCount=0;
+        pendingTicketsCount=0;
+        resolvedTicketsCount=0;
+        rejectedTicketsCount=0;
+        var repo=new TicketRepository();
+        repo.getTicketsWithStatusTypeAndProblemType(function(res){
+            for(let i=0;i<res.length;i++)
+            {
+                if(res[i].assignedTo==id)
+                {    
+                    assignedToMeCount++;
+                }
+                if(res[i].statusType[0].statusType=="Pending")
+                {
+                    pendingTicketsCount++;
+                }
+                if(res[i].statusType[0].statusType=="Resolved")
+                {
+                    resolvedTicketsCount++;
+                }
+                if(res[i].statusType[0].statusType=="Rejected")
+                {
+                    rejectedTicketsCount++;
+                }
+            }
+            resArray=[]
+            let r={};
+            r.TotalTickets=res.length;
+            r.TicketsAssignedToMe=assignedToMeCount;
+            r.PendingTickets=pendingTicketsCount;
+            r.ResolvedTickets=resolvedTicketsCount;
+            r.RejectedTickets=rejectedTicketsCount;
+            resArray.push(r);
+
+            callback(resArray);
+        });  
+    }
+
+    getStatusTickets(id,callback) {
+       var userrepo=new FacilityUserRepository();
+        resArray=[];
+        empArray=[];
+        var role;
+        var repo=new TicketRepository();
+        userrepo.getRole(id,function(r){
+            role=r.role;
+        });
+
+        repo.getTicketsWithStatusTypeAndProblemType(function(res){
+            res.forEach(res=>{
+                res.statusType=res.statusType[0].statusType;
+                res.problemType=res.problemType[0].problemType;
+                res.serviceName=res.serviceName[0].serviceName;
+                resArray.push(res);
+            });
+            resArray.forEach( r=> {
+                userRole.forEach(u => {
+                    if(u.userid == r.createdBy) {
+                        r.createdName = u.name;
+                        r.createdEmail = u.email;
+                    }
+                    if(u.userid == r.assignedTo) {
+                        r.facilityName = u.name;
+                    }
+                });
+            });
+            if(role=="Employee")
+            {
+                resArray.forEach(e=>{
+                    if(e.createdBy==id)
+                    {
+                        empArray.push(e);
+                    }
+                });
+                callback(empArray);
+            }
+            else{
+                callback(resArray);
+            }
         });
     }
     
