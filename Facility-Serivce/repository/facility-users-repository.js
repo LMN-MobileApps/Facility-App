942edfc3ec6998c6a1e4b7305b499c2f2ea0f9e7
rakshahm20 <96164301+rakshahm20@users.noreply.github.com>
2022-01-17 15:52:39 +0530

    Update facility-users-repository.js

--- Facility-Serivce/repository/facility-users-repository.js
+++ Facility-Serivce/repository/facility-users-repository.js
@@ -33,20 +33,18 @@ module.exports = class UserRepository {
             });
         });
     }
+    
     getUserById(id,pass,callback){
         let flag=0;
-        
         for(let i=0;i<user.length;i++)
         {
             if(user[i].userid==id)
             {
                 if(user[i].password==pass){
-                   
                     flag=1;
                 }
             }
         }
-
         if(flag==1)
         {
             callback(true);
@@ -66,6 +64,7 @@ module.exports = class UserRepository {
             }
         }
     }
+    
     getFeedback(callback) {
         const client = MongoClient.connect(mongoCon.mongoURL, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
             if (err) throw err;
@@ -88,35 +87,6 @@ module.exports = class UserRepository {
                     });
         });
     }
-    dashboard(callback){
-        const client = MongoClient.connect(mongoCon.mongoURL, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
-            if (err) throw err;
-            var dbo = db.db(mongoCon.targetDB);
-           
-             dbo.collection(mongoCon.loadCollection.facility_tickets).aggregate([
-                { $lookup:
-                   {
-                     from: mongoCon.loadCollection.ticket_status,
-                     localField: 'statusCode',
-                     foreignField: 'statusCode',
-                     as: 'statusType'
-                   }
-                 },{ $lookup:
-                    {
-                      from: mongoCon.loadCollection.ticket_problem_type,
-                      localField: 'problemCode',
-                      foreignField: 'problemCode',
-                      as: 'problemType'
-                    }
-                  }
-                ]).toArray(function(err, res) {
-                    if (err) throw err;
-                    callback(res);
-                    db.close();
-                  });
-          
-        });
-    }
 
     giveFeedback(obj, callback) {
         const client = MongoClient.connect(mongoCon.mongoURL, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
@@ -132,29 +102,16 @@ module.exports = class UserRepository {
             });
         });
     }
-
-    getFeedback(callback) {
-        const client = MongoClient.connect(mongoCon.mongoURL, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
-            if (err) throw err;
-            var dbo = db.db(mongoCon.targetDB);
     
-            dbo.collection(mongoCon.loadCollection.facility_feedback).aggregate(
-                        [
-                            {
-                              $group: {
-                                  _id: "$ratedTo",
-                                  ratings: { $avg: "$ratings" }
-                                }
-                            }
-                        ]
-                    ).toArray(function(err, res) {
-                        if(err) throw err;
-                        else {
-                            callback(res);
-                        }
-                        db.close();
-                    });
+    getTeamAwards(callback){
+        var resArray=[];
+        lumenStars.forEach(l=>{
+            userRole.forEach(u=>{
+                if(l.userid==u.userid && u.role=="Facility Team"){
+                    resArray.push({"name":u.name,"role":u.role,"awards":l.awards});
+                }
+            });
         });
+        callback(resArray);
     }
-
 } 
